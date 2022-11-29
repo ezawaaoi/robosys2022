7b17bb5b59fd42e12dffe0f419aa35f69e350d7c
江澤碧 <aoi@DESKTOP-COC5RF7.localdomain>
2022-11-29 16:17:29 +0900

    a

--- test.bash
+++ test.bash
@@ -2,22 +2,24 @@
 # SPDX-FileCopyrightText: 2022 Aoi Ezawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng (){
-	echo NG at Line $1
-	res=1
+ng () {
+      echo NG at Line $1
+      res=1
 }
 
+
 res=0
-+res=0
 
-### I/0 TEST ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+  out=$(seq 5 | ./puls)
+  [ "${out}" = 15 ] || ng ${LINENO}
+  
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo | ./plus) #空文字
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+  out=$(echo | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-exit $res
+  exit $res
