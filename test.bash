9ee1595c3fbd805f6594b7e8a066b8a5608da0a3
江澤碧 <aoi@DESKTOP-COC5RF7.localdomain>
2022-11-30 15:08:51 +0900

    update test.bash

--- test.bash
+++ test.bash
@@ -7,18 +7,22 @@ ng () {
       res=1
 }
 
-
 res=0
 
-### I/0 TEST ###  
-  out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+### I/0 ###
+out=$(seq 5 | ./plus)
+[ "${out}" =15 ] || ng ${LINENO}]
 
-  out=$(echo | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+
+### STRANGE INPUT ###  
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-  exit $res
+exit $res
 
