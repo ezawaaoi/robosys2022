ae9acb9f2254d29336e5247b3cbaa2b4c01eaae0
江澤碧 <aoi@DESKTOP-COC5RF7.localdomain>
2022-11-30 17:16:41 +0900

    update a command

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Aoi Ezawa
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
@@ -9,17 +9,18 @@ ng () {
 
 res=0
 
+### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng $LINENO
 
+### STRANGE INPUT ###
 out=$(echo あ | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 out=$(echo | ./plus)
-[ "$?" = 1 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+[ "$?" = 1 ]      || ng $LINENO
+[ "${out}" = "" ] || ng $LINENO
 
 [ "$res" = 0 ] && echo OK
 exit $res
-
